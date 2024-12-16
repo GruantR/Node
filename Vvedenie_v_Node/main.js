8d412e8bb4ac18df359401d6adfc5d6aed2b80ed
Ruslan_Trafimovich <ruworv@gmail.com>
2024-12-16 20:12:30 +0300

    first commit

---
+++ Vvedenie_v_Node/main.js
@@ -0,0 +1,6 @@
+const mathFunctions = require('./math');
+console.log(mathFunctions.add(8, 2));      // Выведет: 10
+console.log(mathFunctions.multiply(8, 2)); // Выведет: 16
+console.log(mathFunctions.subtraction(8,2)); // Выведет 6
+console.log(mathFunctions.division(8,2)); //  Выведет 4
+
